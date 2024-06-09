09d06a8a2e80679d47447993e7389fec247bbab7
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2024-06-08 21:02:04 -0600

    Cors

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8000
 
+// Configura CORS
+app.use(cors({
+    origin: 'https://world-clock-six.vercel.app/', // Reemplaza con el dominio de tu frontend
+    methods: 'GET' // Permite solo métodos GET
+}));
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World from this side')
 });
 
 app.get('/about', (req, res) => {
@@ -13,8 +20,14 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/current-time', async (req, res) => {
+    const timeZone = req.query.timeZone;
+
+    if (!timeZone) {
+        return res.status(400).json({ error: 'timeZone query parameter is required' });
+    }
+
     try {
-        const response = await axios.get('https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam');
+        const response = await axios.get(`https://timeapi.io/api/Time/current/zone?timeZone=${encodeURIComponent(timeZone)}`);
         res.json(response.data);
     } catch (error) {
         console.error('Error fetching the time:', error);
