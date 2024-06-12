b57e1fb0a252735b976039c2da3c7be0d3e33ef9
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2024-06-12 13:33:05 -0600

    Key

--- app.js
+++ app.js
@@ -2,42 +2,52 @@ const express = require('express');
 require('dotenv').config();
 const axios = require('axios');
 const cors = require('cors');
-const { setPolyfills } = require("@growthbook/growthbook");
 const { GrowthBook } = require("@growthbook/growthbook");
 
 const app = express();
 const PORT = 8000;
-const KEY = process.env.clientKey;
 
-console.log("\nTHIS IS THE KEY: " + KEY + " END.\n");
+// Example using Express
+app.use(function(req, res, next) {
+  // Create a GrowthBook instance and store in the request
+  req.growthbook = new GrowthBook({
+    apiHost: "https://cdn.growthbook.io",
+    clientKey: process.env.clientKey
+  });
 
+  // TODO: Add user targeting attributes from cookies, headers, etc.
+  req.growthbook.setAttributes({
+    id: req.user?.id
+  });
 
-setPolyfills({
-  // Required for Node 17 or earlier
-  fetch: require("cross-fetch"),undefined
+  // Clean up at the end of the request
+  res.on('close', () => req.growthbook.destroy());
+
+  // Wait for features to load (will be cached in-memory for future requests)
+  req.growthbook.init({timeout: 1000}).then(() => next())
 });
 
-app.use(function(req, res, next) {
-    // Create a GrowthBook Context
-    req.growthbook = new GrowthBook({
-      apiHost: "https://cdn.growthbook.io",
-      clientKey: KEY,
-      trackingCallback: (experiment, result) => {
-        // TODO: Use your real analytics tracking system
-        console.log("Viewed Experiment", {
-          experimentId: experiment.key,
-          variationId: result.key
-        });
-      }
-    });
-  
-    // Clean up at the end of the request
-    res.on('close', () => req.growthbook.destroy());
-  
-    // Wait for features to load (will be cached in-memory for future requests)
-    req.growthbook.init({ timeout: 1000 })
-      .then(() => next())
-  });
+app.get("/", (req, res) => {
+  const gb = req.growthbook;
+
+  // Get detailed information about the feature evaluation
+  const result = req.growthbook.evalFeature("TestingMode");
+
+  // The value of the feature (or `null` if not defined)
+  console.log(result.value);
+
+  // Why the value was assigned to the user
+  // One of: `override`, `unknownFeature`, `defaultValue`, `force`, or `experiment`
+  console.log(result.source);
+
+  // Boolean on/off flag
+  if (gb.isOn("TestingMode")) {
+    res.send("Modo Testing ACTIVO");
+  }
+  else {
+    res.send("Modo Testing INACTIVO");
+  }
+})
 
 // Middleware personalizado para configurar CORS dinámicamente
 const dynamicCors = (req, res, next) => {
@@ -50,32 +60,6 @@ const dynamicCors = (req, res, next) => {
 
 // Usar el middleware dinámico antes de definir las rutas
 app.use(dynamicCors);
-
-app.get('/', (req, res) => {
-// Get detailed information about the feature evaluation
-const result = req.growthbook.evalFeature("TestingMode");
-
-// The value of the feature (or `null` if not defined)
-console.log(result.value);
-
-// Why the value was assigned to the user
-// One of: `override`, `unknownFeature`, `defaultValue`, `force`, or `experiment`
-console.log(result.source);
-
-// The string id of the rule (if any) which was used
-console.log(result.ruleId);
-
-// Information about the experiment (if any) which was used
-console.log(result.experiment);
-
-
-    if (req.growthbook.isOn("TestingMode")) {
-        res.send("Modo Testing ACTIVO");
-      }
-      else {
-        res.send("Modo Testing INACTIVO");
-      }
-});
 
 app.get('/current-time', async (req, res) => {
     const timeZone = req.query.timeZone;
