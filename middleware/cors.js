7f76c531957493ff80a66ef2ee704dcb8b9490a2
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2024-06-22 10:39:32 -0600

    cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,7 +4,8 @@ const dynamicCors = (req, res, next) => {
   if (req.growthbook.isOn('TestingMode')) {
     cors()(req, res, next);
   } else {
-    cors({ origin: process.env.origin, methods: 'GET' })(req, res, next);
+    //cors({ origin: process.env.origin, methods: 'GET' })(req, res, next);
+    cors()(req, res, next);
   }
 };
 
