84ffc4d293603d3b1c3342734026d12dd6dfa33f
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2024-06-22 10:50:46 -0600

    cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,12 +1,26 @@
 const cors = require('cors');
 
 const dynamicCors = (req, res, next) => {
-  if (req.growthbook.isOn('TestingMode')) {
-    cors()(req, res, next);
-  } else {
-    //cors({ origin: process.env.origin, methods: 'GET' })(req, res, next);
-    cors()(req, res, next);
-  }
+  // Configuración de CORS específica para TestingMode
+  const testingCorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  // Configuración de CORS específica para producción
+  const productionCorsOptions = {
+    origin: process.env.origin,  // Asegúrate de que esta variable de entorno está configurada correctamente
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  // Seleccionar configuración de CORS basada en la condición
+  const corsOptions = req.growthbook.isOn('TestingMode') ? testingCorsOptions : productionCorsOptions;
+
+  cors(corsOptions)(req, res, next);
 };
 
 module.exports = dynamicCors;
